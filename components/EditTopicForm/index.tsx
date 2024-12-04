3289da6cd6d37a5aacd6ace8f8519f398a639326
avangardewashere <avelpanaligan@gmail.com>
2024-12-04 19:16:31 +0800

    add/edit-topic

---
+++ components/EditTopicForm/index.tsx
@@ -0,0 +1,12 @@
+export default function EditTopicForm(){
+    return(
+        <form className="flex flex-col gap-3">
+        <input type="text" placeholder="Topic Title" className="border rounded-md border-slate-500 px-8 py-2" />
+        <input type="text" placeholder="Topic Description" className="border rounded-md border-slate-500 px-8 py-2" />
+
+        <button className="bg-green-600 font-bold rounded-md text-white py-3 px-6 w-fit">
+           Update Topic
+        </button>
+     </form>
+    )
+}
