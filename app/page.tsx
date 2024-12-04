aa4740926a4ad3ee3f2010fb01be366f434a9732
avangardewashere <avelpanaligan@gmail.com>
2024-12-04 18:50:07 +0800

    added/card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import TopicList from '@/components/TopicList'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-   <div>Hello</div>
+  <TopicList/>
   )
 }
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Navbar() {
     return(
-        <nav>
-            <Link href="/">Ideas There</Link>
-            <Link href={"/addTopic"}>Add Topic</Link>
+        <nav className="flex justify-between items-center bg-slate-800 px-8 py-3 rounded-md">
+            <Link className="text-white font-bold" href="/">Ideas There</Link>
+            <Link className="bg-white p-2" href={"/addTopic"}>Add Topic</Link>
         </nav>
     )
 }
