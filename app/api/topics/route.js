a7c0ab10055c25db347474cb27bd4c0f1f53250f
avangardewashere <avelpanaligan@gmail.com>
2024-12-04 21:38:29 +0800

    fetch/data-from -mongo

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -1,12 +1,25 @@
 import connectMongoDB from "../../libs/mongodb";
-import Topic from '@/app/models/topic'
+import Topic from "@/app/models/topic";
 import { NextResponse } from "next/server";
 
-export async function POST(request){
-    const {title,description} = request.json();
+export async function POST(request) {
+  const { title, description } = await request.json();
 
+  await connectMongoDB();
+
+  await Topic.create({ title, description });
+  return NextResponse.json({ message: "Topic has been Created " + title +" .",createdWhat:title,also:description }, { status: 201 });
+}
+
+export async function GET() {
+  await connectMongoDB();
+  const topics = await Topic.find();
+  return NextResponse.json({ topics });
+}
+
+export async function DELETE(request){
+    const id = request.nextUrl.searchParams.get("id");
     await connectMongoDB();
-
-    await Topic.create({title,description})
-    return NextResponse.json({message:"Topic Create"},{status:201})
+    await Topic.findByIdAndDelete(id);
+    return NextResponse.json({message:"Topic Deleted"},{status:200});
 }
--- components/TopicList/index.tsx
+++ components/TopicList/index.tsx
@@ -2,22 +2,44 @@ import Link from "next/link";
 import RemoveButton from "../fragments/removeButton";
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicList() {
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3001/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (err) {
+    console.log("Error loading topics: ", err);
+  }
+};
+
+export default async function TopicList() {
+  const   {topics}  = await getTopics();
+ 
+    
   return (
-    <>
-      <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+    <>{topics?.map((item:any)=>(
+        <div key={item?.id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
         <div>
-          <h2 className="font-bold text-2xl">Topic Title</h2>
-          <div>Topic Description</div>
+          <h2 className="font-bold text-2xl">{item.title ?? "Topic Title"}</h2>
+          <div>{item.description ?? "Topic Description"}</div>
         </div>
 
         <div className="flex gap-2">
           <RemoveButton />
-          <Link href={"/editTopic/123"}>
+          <Link href={`/editTopic/${item.id}`}>
             <HiPencilAlt size={24} />
           </Link>
         </div>
       </div>
+
+    ))}
+     
     </>
   );
 }
