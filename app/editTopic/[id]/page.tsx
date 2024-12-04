0c22ad75966cf6965f8fe2e90438acd9e59975fa
avangardewashere <avelpanaligan@gmail.com>
2024-12-05 06:38:34 +0800

    add/update-form-use-client

--- app/editTopic/[id]/page.tsx
+++ app/editTopic/[id]/page.tsx
@@ -1,10 +1,29 @@
 import EditTopicForm from "@/components/EditTopicForm";
 
-export default function EditTopic ({params}:any){
+const getTopicById = async (id: number) => {
+  try {
+    const res = await fetch(`https://localhost:3000/api/topics/${id}`, {
+      cache: "no-store",
+    });
 
-    const{id} = params;
-    console.log("id:",id);
-    return(
-        <EditTopicForm />
-    )
+    if (!res.ok) {
+      throw new Error("Failed to fetch topi");
+    }
+
+    const data = await res.json();
+    return data;
+
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default async function EditTopic({ params }: any) {
+  const { id } = params;
+  console.log("id:", id);
+
+  const { topic } = await getTopicById(id);
+  const { title, description } = topic;
+  console.log("id :", id);
+  return <EditTopicForm id={id} title={title} description={description} />;
 }
--- components/EditTopicForm/index.tsx
+++ components/EditTopicForm/index.tsx
@@ -1,8 +1,23 @@
-export default function EditTopicForm(){
+"use client"
+
+import { useState } from "react";
+
+interface EditTopicFormProps{
+    id:string
+    title:string
+    description:string
+}
+
+export default function EditTopicForm(props:EditTopicFormProps){
+    const {title,id, description} = props;
+
+    const [newTitle,setNewTitle] = useState(title);
+    const [newDescription,setNewDescription] = useState(description);
+
     return(
         <form className="flex flex-col gap-3">
-        <input type="text" placeholder="Topic Title" className="border rounded-md border-slate-500 px-8 py-2" />
-        <input type="text" placeholder="Topic Description" className="border rounded-md border-slate-500 px-8 py-2" />
+        <input onChange={(e)=>setNewTitle(e.target.value)} value={title} type="text" placeholder="Topic Title" className="border rounded-md border-slate-500 px-8 py-2" />
+        <input onChange={(e)=>setNewDescription(e.target.value)} value={description} type="text" placeholder="Topic Description" className="border rounded-md border-slate-500 px-8 py-2" />
 
         <button className="bg-green-600 font-bold rounded-md text-white py-3 px-6 w-fit">
            Update Topic
