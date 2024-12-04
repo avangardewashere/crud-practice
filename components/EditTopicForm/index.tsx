94d6946e87624b3232840e7b041dbf0e85333a1a
avangardewashere <avelpanaligan@gmail.com>
2024-12-05 06:43:44 +0800

    done/crud-test/for-testing

--- components/EditTopicForm/index.tsx
+++ components/EditTopicForm/index.tsx
@@ -1,27 +1,62 @@
-"use client"
+"use client";
 
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-interface EditTopicFormProps{
-    id:string
-    title:string
-    description:string
+interface EditTopicFormProps {
+  id: string;
+  title: string;
+  description: string;
 }
 
-export default function EditTopicForm(props:EditTopicFormProps){
-    const {title,id, description} = props;
+export default function EditTopicForm(props: EditTopicFormProps) {
+  const { title, id, description } = props;
+  const router = useRouter();
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
 
-    const [newTitle,setNewTitle] = useState(title);
-    const [newDescription,setNewDescription] = useState(description);
+  const handleSubmit = async (e:any) =>{
+    e.prevenrDefault()
 
-    return(
-        <form className="flex flex-col gap-3">
-        <input onChange={(e)=>setNewTitle(e.target.value)} value={title} type="text" placeholder="Topic Title" className="border rounded-md border-slate-500 px-8 py-2" />
-        <input onChange={(e)=>setNewDescription(e.target.value)} value={description} type="text" placeholder="Topic Description" className="border rounded-md border-slate-500 px-8 py-2" />
+    try{
+        const res= await fetch(`httpL//localhost/3001/api/topics/${id}`,{
+            method:"PUT",
+            headers:{
+                "Contetnt-tye":"application/json",
+            },
+            body:JSON.stringify({newTitle,newDescription})
+        })
 
-        <button className="bg-green-600 font-bold rounded-md text-white py-3 px-6 w-fit">
-           Update Topic
-        </button>
-     </form>
-    )
+        if(!res.ok){
+            throw new Error("Failed to update topic")
+        }
+
+        router.push("/")
+    }catch(err){
+        console.log(err)
+    }
+  }
+
+  return (
+    <form className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
+        type="text"
+        placeholder="Topic Title"
+        className="border rounded-md border-slate-500 px-8 py-2"
+      />
+      <input
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
+        type="text"
+        placeholder="Topic Description"
+        className="border rounded-md border-slate-500 px-8 py-2"
+      />
+
+      <button onClick={handleSubmit} className="bg-green-600 font-bold rounded-md text-white py-3 px-6 w-fit">
+        Update Topic
+      </button>
+    </form>
+  );
 }
